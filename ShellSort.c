b9ceb998bc26a50f8cc329bdb884ad032e0fe42f
Swarup Deb <55588687+SwarupDeb@users.noreply.github.com>
2021-10-02 18:12:12 +0530

    Update ShellSort.c

--- ShellSort.c
+++ ShellSort.c
@@ -1,45 +1,73 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void shellSort(int data[], int size)
-{
-    int i, j, gap;
-    for (gap = (size / 2); gap >= 1; gap = (gap / 2))
-    {
-        for (j = gap; j < size; j++)
-        {
-            for (i = (j - gap); i >= 0; i = (i - gap))
-            {
-                if (data[i + gap] > data[i])
-                {
-                    break;
-                }
-                else
-                {
-                    int temp;
-                    temp = data[i + gap];
-                    data[i + gap] = data[i];
-                    data[i] = temp;
-                }
-            }
-        }
-    }
-}
-void transversal(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-}
-int main()
-{
-    int arr[] = {50, 96, 67, 12, 31, 55, 1, 38, 10, 2};
-    int size = sizeof(arr) / sizeof(int);
-    printf("Before Insertion Sort\n");
-    transversal(arr, size);
+#define MAX 7
 
-    shellSort(arr, size);
-    printf("\nAfter Insertion sort\n");
-    transversal(arr, size);
-    return 0;
+int intArray[MAX] = {4,6,3,2,1,9,7};
+
+void printline(int count) {
+   int i;
+	
+   for(i = 0;i < count-1;i++) {
+      printf("=");
+   }
+	
+   printf("=\n");
+}
+
+void display() {
+   int i;
+   printf("[");
+	
+   for(i = 0;i < MAX;i++) {
+      printf("%d ",intArray[i]);
+   }
+	
+   printf("]\n");
+}
+
+void shellSort() {
+   int inner, outer;
+   int valueToInsert;
+   int interval = 1;   
+   int elements = MAX;
+   int i = 0;
+   
+   while(interval <= elements/3) {
+      interval = interval*3 +1;
+   }
+
+   while(interval > 0) {
+      printf("iteration %d#:",i); 
+      display();
+      
+      for(outer = interval; outer < elements; outer++) {
+         valueToInsert = intArray[outer];
+         inner = outer;
+			
+         while(inner > interval -1 && intArray[inner - interval] 
+            >= valueToInsert) {
+            intArray[inner] = intArray[inner - interval];
+            inner -=interval;
+            printf(" item moved :%d\n",intArray[inner]);
+         }
+         
+         intArray[inner] = valueToInsert;
+         printf(" item inserted :%d, at position :%d\n",valueToInsert,inner);
+      }
+		
+      interval = (interval -1) /3;
+      i++;
+   }          
+}
+
+int main() {
+   printf("Input Array: ");
+   display();
+   printline(50);
+   shellSort();
+   printf("Output Array: ");
+   display();
+   printline(50);
+   return 1;
 }
